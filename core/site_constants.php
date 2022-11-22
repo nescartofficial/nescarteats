289585aef86cbe9787cb5e47e74c4496b8219b02
Chris Asek <chrisasek@gmail.com>
2022-11-22 17:02:56 +0100

    Update site_constants.php

--- core/site_constants.php
+++ core/site_constants.php
@@ -2,11 +2,14 @@
 defined('DS') ? NULL : define('DS', DIRECTORY_SEPARATOR);
 
 defined('SITE_NAME') ? NULL : define('SITE_NAME', 'Nescart Eats');
-defined('SITE_URL') ? NULL : define('SITE_URL', '/website/nescarteats.com/');
+defined('VENDOR_URL') ? NULL : define('VENDOR_URL', 'https://vendor.nescarteats.com/');
+defined('AGENT_URL') ? NULL : define('AGENT_URL', 'https://agent.nescarteats.com/');
+defined('SITE_URL') ? NULL : define('SITE_URL', 'https://nescarteats.azurewebsites.net/');
 defined('SITE_ADMIN_URL') ? NULL : define('SITE_ADMIN_URL', SITE_URL . 'administrator/');
 
-defined('SITE_ROOT') ? NULL : define('SITE_ROOT', 'C:' . DS . 'xampp' . DS . 'htdocs' . DS . 'website' . DS . "nescarteats.com");
+defined('SITE_ROOT') ? NULL : define('SITE_ROOT', DS . 'home');
 defined('SITE_ADMIN_ROOT') ? NULL : define('SITE_ADMIN_ROOT', SITE_ROOT . DS . 'administrator');
+
 defined('LIB_PATH') ? NULL : define('LIB_PATH', SITE_ROOT . DS . "models");
 defined('CORE_PATH') ? NULL : define('CORE_PATH', SITE_ROOT . DS . "core");
 defined('ASSET_PATH') ? NULL : define('ASSET_PATH', SITE_ROOT . DS . "assets");
